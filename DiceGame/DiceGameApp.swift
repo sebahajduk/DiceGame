e320fbf5f5cb8da4c9ca8d6ca8429602ef078eb2
Sebastian Hajduk <shajduk0@gmail.com>
2022-06-25 12:04:34 +0200

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Sebastian Hajduk on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
